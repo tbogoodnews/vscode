70cffa1c29c1fda913d99bf3269dfe1abc917ddd
deepak1556 <hop2deep@gmail.com>
2026-05-01 05:19:07 +0900

    feat: update to Electron v42

--- src/vs/platform/browserView/electron-main/browserView.ts
+++ src/vs/platform/browserView/electron-main/browserView.ts
@@ -94,7 +94,7 @@ export class BrowserView extends Disposable {
 	) {
 		super();
 
-		const webPreferences: Electron.WebPreferences & { type: ReturnType<Electron.WebContents['getType']> } = {
+		const webPreferences: Electron.WebPreferences = {
 			...options?.webPreferences,
 
 			nodeIntegration: false,
@@ -107,9 +107,6 @@ export class BrowserView extends Disposable {
 
 			webviewTag: false,
 			session: this.session.electronSession,
-
-			// TODO@kycutler: Remove this once https://github.com/electron/electron/issues/42578 is fixed
-			type: 'browserView'
 		};
 
 		this._view = new WebContentsView({
